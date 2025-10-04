8f225183494f52cfb7f49eb822cd6d1eb48e27d2
Deyvison Jose De Paula <EX161266@edp.com.br>
2025-10-04 18:31:46 -0300

    ORM - SaleRepo e SaleItemRepo IoC- Registro das Repositories Sale e SalesItem

---
+++ backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/SaleItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories;
+
+public class SaleItemRepository : ISaleItemRepository
+{
+    public Task<SaleItem> CreateAsync(SaleItem item, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<SaleItem>> GetBySaleIdAsync(Guid saleId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/SaleRepository.cs
@@ -0,0 +1,48 @@
+﻿using Ambev.DeveloperEvaluation.Common.Security;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories;
+
+public class SaleRepository : ISaleRepository
+{
+    private readonly DefaultContext _context;
+
+    public SaleRepository(DefaultContext context)
+    {
+        _context = context;
+    }
+    public async Task<Sale> CreateAsync(Sale sale, CancellationToken cancellationToken)
+    {
+        await _context.Sales.AddAsync(sale, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        return sale;
+    }
+    public async Task<List<Sale>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await _context.Sales
+            .Include(s => s.Items)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<Sale?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _context.Sales
+            .Include(s => s.Items)
+            .FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
+    }
+    public async Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        Sale sale = await _context.Sales
+            .FirstOrDefaultAsync(s => s.Id == id, cancellationToken)
+            .ConfigureAwait(false);
+
+        if (sale == null)
+            return false;
+
+        _context.Sales.Remove(sale);
+        var affected = await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        return affected > 0;
+    }
+}
